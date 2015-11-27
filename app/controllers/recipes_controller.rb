785e1b2b66555a66588ec4a2b50e0c7e8b66a06f
Lalith <lalithr95@gmail.com>
2015-11-27 21:25:53 +0530

    basic functionality for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
 
 	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 	def index
+		@recipe = Recipe.all.order("created_at DESC")
 	end
 
 	def new
@@ -11,8 +12,38 @@ class RecipesController < ApplicationController
 	def show
 	end
 
+	def create
+		@recipe = Recipe.new(recipe_params)
+		if @recipe.save
+			redirect_to @recipe, notice: "Created new Recipe"
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @recipe.update(recipe_params)
+			redirect_to @recipe
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		if @recipe.destroy
+			redirect_to root_path, notice: 'Successfully deleted'
+		end
+	end
+
 	private
 	def find_recipe
 		@recipe = Recipe.find(params[:id])
+	end
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :description)
 	end
 end
